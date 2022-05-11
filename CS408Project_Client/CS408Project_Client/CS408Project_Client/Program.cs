d7220c78cb62ab154cd9cb19961a9afba41fc043
denizumuteser <denizumuteser@yahoo.com.tr>
2022-05-11 21:06:46 +0300

    todelete

---
+++ CS408Project_Client/CS408Project_Client/CS408Project_Client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CS408Project_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
