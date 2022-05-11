895c6abe627e0d765a82e3c1e0553dad780ed28e
denizumuteser <denizumuteser@yahoo.com.tr>
2022-05-11 23:32:36 +0300

    First iteration of Server

--- CS408Project_Server/CS408Project_Server/CS408Project_Server/Form1.Designer.cs
+++ CS408Project_Server/CS408Project_Server/CS408Project_Server/Form1.Designer.cs
@@ -29,13 +29,68 @@ namespace CS408Project_Server
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.richTextBox_Console = new System.Windows.Forms.RichTextBox();
+            this.textBox_Port = new System.Windows.Forms.TextBox();
+            this.label_port = new System.Windows.Forms.Label();
+            this.button_Listen = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox_Console
+            // 
+            this.richTextBox_Console.Location = new System.Drawing.Point(24, 77);
+            this.richTextBox_Console.Name = "richTextBox_Console";
+            this.richTextBox_Console.Size = new System.Drawing.Size(749, 353);
+            this.richTextBox_Console.TabIndex = 0;
+            this.richTextBox_Console.Text = "";
+            // 
+            // textBox_Port
+            // 
+            this.textBox_Port.Location = new System.Drawing.Point(59, 39);
+            this.textBox_Port.Name = "textBox_Port";
+            this.textBox_Port.Size = new System.Drawing.Size(119, 20);
+            this.textBox_Port.TabIndex = 1;
+            // 
+            // label_port
+            // 
+            this.label_port.AutoSize = true;
+            this.label_port.Location = new System.Drawing.Point(21, 42);
+            this.label_port.Name = "label_port";
+            this.label_port.Size = new System.Drawing.Size(32, 13);
+            this.label_port.TabIndex = 2;
+            this.label_port.Text = "Port: ";
+            // 
+            // button_Listen
+            // 
+            this.button_Listen.Location = new System.Drawing.Point(184, 37);
+            this.button_Listen.Name = "button_Listen";
+            this.button_Listen.Size = new System.Drawing.Size(75, 23);
+            this.button_Listen.TabIndex = 3;
+            this.button_Listen.Text = "Listen";
+            this.button_Listen.UseVisualStyleBackColor = true;
+            this.button_Listen.Click += new System.EventHandler(this.button_Listen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.button_Listen);
+            this.Controls.Add(this.label_port);
+            this.Controls.Add(this.textBox_Port);
+            this.Controls.Add(this.richTextBox_Console);
+            this.Name = "Form1";
+            this.Text = "Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox_Console;
+        private System.Windows.Forms.TextBox textBox_Port;
+        private System.Windows.Forms.Label label_port;
+        private System.Windows.Forms.Button button_Listen;
     }
 }
 
--- CS408Project_Server/CS408Project_Server/CS408Project_Server/Form1.cs
+++ CS408Project_Server/CS408Project_Server/CS408Project_Server/Form1.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +16,199 @@ namespace CS408Project_Server
 {
     public partial class Form1 : Form
     {
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        List<Socket> clientSockets = new List<Socket>();
+
+        bool terminating = false;
+        bool listening = false;
+
         public Form1()
         {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
             InitializeComponent();
+        }
+
+        private void button_Listen_Click(object sender, EventArgs e)
+        {
+            string port = textBox_Port.Text;
+            int portNum;
+            if (!Int32.TryParse(port, out portNum))
+            {//port is not a number
+                richTextBox_Console.AppendText("Check the port number!\n");
+                return;
+            }
+            //start listening
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, portNum);
+            serverSocket.Bind(endPoint);
+            serverSocket.Listen(3);
+            richTextBox_Console.AppendText("Started listening on port: " + port + ".\n");
+
+            listening = true;
+            button_Listen.Enabled = false;
+
+            Thread acceptThread = new Thread(Accept);
+            acceptThread.Start();
+        }
+
+        private void Accept()
+        {
+            while (listening)
+            {
+                try
+                {//listening
+                    Socket newClient = serverSocket.Accept();
+                    clientSockets.Add(newClient);
+                    Thread receiveThread = new Thread(() => Receive(newClient));
+                    receiveThread.Start();
+                }
+                catch
+                {
+                    if (terminating)
+                    {
+                        listening = false;
+                    }
+                    else
+                    {
+                        richTextBox_Console.AppendText("The socket stopped working.\n");
+                    }
+
+                }
+            }
+        }
+
+        private void Receive(Socket thisClient)
+        {
+            bool connected = true;
+
+            while (connected && !terminating)
+            {
+                try
+                {
+                    Byte[] buffer = new Byte[64]; //byte array
+                    thisClient.Receive(buffer);
+
+                    string incomingMessage = Encoding.Default.GetString(buffer);
+                    incomingMessage = incomingMessage.Substring(0, incomingMessage.IndexOf('\0'));
+                    String[] messages = incomingMessage.Split('|');
+                    //classify coming message according to header
+                    if (messages[0] == "CONNECT_REQUEST")
+                    {
+                        string username = messages[1];
+
+                        //if user is already registered
+                        if (isUsernameExist(username))
+                        {
+                            //connected
+                            //send message to server console
+                            richTextBox_Console.AppendText(username + "has connected.\n");
+                            //send message to client
+                            Byte[] logtoclient = Encoding.Default.GetBytes("1|Hello" + username + "! You are connected to the server.\n");
+                            thisClient.Send(logtoclient);  
+                        }
+                        else
+                        {
+                            //cannot connect
+                            //send message to server console
+                            richTextBox_Console.AppendText(username + " tried to connect to the server but cannot!.\n");
+                            //send message to client
+                            Byte[] logtoclient = Encoding.Default.GetBytes("0|Please enter a valid username\n");
+                            thisClient.Send(logtoclient);
+                        }
+
+                    }
+                    else if (messages[0] == "POST")
+                    {
+                        string username = messages[1];
+                        string post = messages[2];
+                        string timestamp = messages[3];
+                        //handle post
+                        handlePost(username, post, timestamp);
+                    }
+                    else if(messages[0] == "RETRIEVE_ALL")
+                    {
+                        //print to server console
+                        string username = messages[1];
+                        richTextBox_Console.AppendText("Showed all posts for "+username+".\n");
+                        //handle post retriever
+                        postRetriever(username, thisClient);
+                    }
+                }
+                catch
+                {
+                    if (!terminating)
+                    {
+                        richTextBox_Console.AppendText("Client has disconnected.\n");
+                    }
+                    thisClient.Close();
+                    clientSockets.Remove(thisClient);
+                    connected = false;
+                }
+            }
+        }
+
+        private void postRetriever(string username, Socket thisclient)
+        {
+            string all_posts = "";
+            //get posts from log
+            var lines = File.ReadLines(@"database.txt");
+            foreach (var line in lines)
+            {
+                all_posts += line+"\n";
+            }
+            //send posts to client
+            Byte[] logtoclient = Encoding.Default.GetBytes("2$"+all_posts);
+            thisclient.Send(logtoclient);
+        }
+
+        private void handlePost(string username, string post, string timestamp)
+        {
+            //print server console
+            richTextBox_Console.AppendText(username + "has sent a post:\n");
+            richTextBox_Console.AppendText(post+"\n");
+            //add post to posts.log
+            using (StreamWriter w = File.AppendText("posts.log"))
+            {
+                w.WriteLine(username+"|"+(getLastPostId()+1)+post+"|"+timestamp);
+            }
+        }
+
+        private int getLastPostId()
+        {
+            int lastid = 0;
+            var lines = File.ReadLines(@"posts.log");
+            foreach (var line in lines)
+            {
+                lastid += 1;
+            }
+            return lastid;
+        }
+
+        private void Form1_FormClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {// Avoid crash at closing
+            listening = false;
+            terminating = true;
+
+            foreach (var socket in clientSockets)
+            {
+                Byte[] logtoclient = Encoding.Default.GetBytes("3|Server has disconnected!");
+                socket.Send(logtoclient);
+            }
+            Environment.Exit(0); //release environment resources
+        }
+
+        private bool isUsernameExist(string username)
+        {
+            var lines = File.ReadLines(@"database.txt");
+            foreach (var line in lines)
+            {
+                if (username == line)
+                {//found matching username in database
+                    return true;
+                }
+            }
+            //not found username in database
+            return false;
         }
     }
 }
